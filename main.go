50c181939e65853484e88e8a35afdd36a0013a0b
jdjoum <jdjoum@hotmail.com>
2024-05-13 17:20:08 -0400

    start golang-api-project

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+	{ID: "4", Title: "48 Laws of Power", Author: "Robert Greene", Quantity: 1},
 }
 
 func getBooks() {
